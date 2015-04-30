632573f1bd14d039910d39d0da14ebf438e45bb0
Alexander Putilin <avp-13@yandex.ru>
2015-04-30 20:55:13 +0300

    Serve static files in production using whitenoise/gunicorn

--- myflair/settings.py
+++ myflair/settings.py
@@ -107,3 +107,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'myflair', 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
